d995d6918f341f3984feb3064312db9dc82a07a1
Constantin Wiemer <constantin.wiemer@gmx.de>
2016-05-14 22:07:42 +0200

    Reads part of the Amiga hunk format (code without data and relocations), executes a simple test program (two numbers are added in a loop)

---
+++ cwtest.c
@@ -0,0 +1,7 @@
+void cwtest()
+{
+    int i, j;
+    for (i = 1; i <= 3; i++)
+        j += i;
+    return;
+}
--- vade.cxx
+++ vade.cxx
@@ -67,24 +67,28 @@ void m68k_instr_callback()
 {
     char instr[100];
     std::string dump;
-    unsigned int pc;
+    unsigned int ipc, pc;
     unsigned int nbytes;
 
-    pc = m68k_get_reg(NULL, M68K_REG_PC);
-    nbytes = m68k_disassemble(instr, pc, M68K_CPU_TYPE_68000);
+    ipc = pc = m68k_get_reg(NULL, M68K_REG_PC);
+    nbytes = m68k_disassemble(instr, ipc, M68K_CPU_TYPE_68000);
     while (nbytes > 0) {
         dump += Poco::format("%04x ", m68k_read_disassembler_16(pc));
         nbytes -= 2;
         pc += 2;
     }
-    LOG4CXX_DEBUG(g_logger, Poco::format("next instruction at 0x%06x: %-20s: %s", pc, dump, std::string(instr)));
+    LOG4CXX_DEBUG(g_logger, Poco::format("next instruction at 0x%06x: %-20s: %s", ipc, dump, std::string(instr)));
 }
 
 
+// TODO: throw exception in case of illegal memory access
 unsigned int m68k_read_memory_8(unsigned int address)
 {
-    LOG4CXX_DEBUG(g_logger, Poco::format("8 bit read from address 0x%08x - NOT IMPLEMENTED", address));
-    return 0x55;
+    LOG4CXX_DEBUG(g_logger, Poco::format("8 bit read from address 0x%08x", address));
+    if ((address >= ADDR_MEM_START) && (address <= ADDR_MEM_END))
+        return READ_BYTE(g_mem, address);
+    else
+        return 0x55;
 }
 
 unsigned int m68k_read_memory_16(unsigned int address)
@@ -162,19 +166,50 @@ void m68k_write_memory_32(unsigned int address, unsigned int value)
 };
 
 
+//
+// generate a hexdump from a buffer of bytes
+//
+std::string hexdump (const uint8_t *buffer, size_t length)
+{
+    std::string dump;
+    size_t pos = 0;
+
+    while (pos < length)
+    {
+        dump += Poco::format ("%04?x: ", pos);
+        std::string line;
+        for (size_t i = pos; (i < pos + 16) && (i < length); i++)
+        {
+            dump += Poco::format ("%02?x ", buffer[i]);
+            if (buffer[i] >= 0x20 && buffer[i] <= 0x7e)
+            {
+                line.append (1, buffer[i]);
+            }
+            else
+            {
+                line.append (1, '.');
+            }
+        }
+        if (line.size() < 16)
+            dump.append (3 * (16 - line.size()), ' ');
+
+        dump.append (1, '\t');
+        dump += line;
+        dump.append (1, '\n');
+        pos += 16;
+    }
+    return dump;
+}
+
+
 int main(int argc, char *argv[])
 {
     // setup logging
     g_logger = log4cxx::Logger::getLogger("tftpd");
     log4cxx::PropertyConfigurator::configure("logging.properties");
 
-/*
-    // allocate memory
-    if (!(g_mem = (uint8_t *) malloc(ADDR_MEM_END - ADDR_MEM_START + 1))) {
-        LOG4CXX_ERROR(g_logger, "could not allocate memory");
-        return 1;
-    }
-*/
+    // allocate memory for our VM
+    g_mem = new uint8_t[ADDR_MEM_END - ADDR_MEM_START + 1];
 
     //
     // load executable
@@ -185,23 +220,71 @@ int main(int argc, char *argv[])
     }
     try
     {
-        Poco::FileInputStream is(argv[1]);
-        Poco::BinaryReader::BinaryReader reader(is, Poco::BinaryReader::BIG_ENDIAN_BYTE_ORDER);
+        Poco::FileInputStream exe(argv[1]);
+        Poco::BinaryReader::BinaryReader reader(exe, Poco::BinaryReader::BIG_ENDIAN_BYTE_ORDER);
         uint32_t htype;
-        reader >> htype;
-        LOG4CXX_INFO(g_logger, "hunk type = " << htype);
-        switch (htype)
-        {
-            case HUNK_HEADER:
-                break;
+        while (!reader.eof()) {
+            reader >> htype;
+            switch (htype)
+            {
+                case HUNK_HEADER:
+                    LOG4CXX_INFO(g_logger, "hunk type = HUNK_HEADER");
+                    uint32_t lword;
+                    reader >> lword;
+                    LOG4CXX_DEBUG(g_logger, "long words reserved for resident libraries: " << lword);
+                    reader >> lword;
+                    LOG4CXX_DEBUG(g_logger, "number of hunks: " << lword);
+                    uint32_t fhunk;
+                    reader >> fhunk;
+                    LOG4CXX_DEBUG(g_logger, "number of first hunk: " << fhunk);
+                    uint32_t lhunk;
+                    reader >> lhunk;
+                    LOG4CXX_DEBUG(g_logger, "number of last hunk: " << lhunk);
+                    for (int i = fhunk; i <= lhunk; i++) {
+                        reader >> lword;
+                        LOG4CXX_DEBUG(g_logger, "size (in bytes) of hunk #" << i << ": " << lword * 4);
+                    }
+                    break;
+
+                case HUNK_CODE:
+                    LOG4CXX_INFO(g_logger, "hunk type = HUNK_CODE");
+                    uint32_t nwords;
+                    reader >> nwords;
+                    LOG4CXX_DEBUG(g_logger, "size (in bytes) of code hunk: " << nwords * 4);
+                    reader.readRaw((char *) g_mem + ADDR_CODE_START, nwords * 4);
+                    LOG4CXX_TRACE(g_logger, "hex dump of code:\n" << hexdump(g_mem + ADDR_CODE_START, nwords * 4));
+                    break;
+
+                case HUNK_DATA:
+                    LOG4CXX_INFO(g_logger, "hunk type = HUNK_DATA");
+                    LOG4CXX_ERROR(g_logger, "hunk type is not implemented");
+                    break;
+
+                case HUNK_BSS:
+                    LOG4CXX_INFO(g_logger, "hunk type = HUNK_BSS");
+                    LOG4CXX_ERROR(g_logger, "hunk type is not implemented");
+                    break;
+
+                case HUNK_RELOC32:
+                    LOG4CXX_INFO(g_logger, "hunk type = HUNK_RELOC32");
+                    LOG4CXX_ERROR(g_logger, "hunk type is not implemented");
+                    break;
+
+                case HUNK_END:
+                    LOG4CXX_INFO(g_logger, "hunk type = HUNK_END");
+                    break;
+
+                default:
+                    LOG4CXX_ERROR(g_logger, "unknown hunk type: " << htype);
+            }
         }
     }
     catch (std::exception &e)
     {
-        LOG4CXX_ERROR(g_logger, "exception occurred while loading executable: " << e.what());
+        LOG4CXX_FATAL(g_logger, "exception occurred while loading executable: " << e.what());
         return 1;
     }
-    return 0;
+
 /*
     // test code (in reversed byte oder)
     uint16_t *p = (uint16_t *) (g_mem + ADDR_CODE_START);
@@ -215,12 +298,11 @@ int main(int argc, char *argv[])
         *p = 0x714e;
 */
 
-/*
     m68k_init();
     m68k_set_cpu_type(M68K_CPU_TYPE_68000);
     m68k_pulse_reset();
-    m68k_execute(100);
-*/
+    m68k_execute(1000);
+    LOG4CXX_TRACE(g_logger, "stack after execution of the program:\n" << hexdump(g_mem + ADDR_STACK_END - 32, 32));
 
     return 0;
 }
