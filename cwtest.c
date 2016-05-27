16a4ed1fcb8eac328560cbe145d6fee816fcf462
Constantin Wiemer <constantin.wiemer@gmx.de>
2016-05-27 22:44:49 +0200

    Changed the test program (converts a string to upper case)

--- cwtest.c
+++ cwtest.c
@@ -1,7 +1,13 @@
+// The string needs to be defined as global *array* (not pointer), otherwise GCC will put it at the
+// *beginning* of the code section => maybe a bug in GCC?
+static char s[] = "hello world";
+
 void cwtest()
 {
-    int i, j;
-    for (i = 1; i <= 3; i++)
-        j += i;
+    char *p;
+    for (p = s; *p != 0; p++) {
+        if (*p >= 97 && *p <= 122)
+            *p -= 32;
+    }
     return;
 }
