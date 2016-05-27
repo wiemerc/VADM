6c64a6b3b9138ba5e084a176667acedb484f32a6
Constantin Wiemer <constantin.wiemer@gmx.de>
2016-05-27 22:41:53 +0200

    Implemented relocation and data blocks

--- vade.cxx
+++ vade.cxx
@@ -11,6 +11,7 @@
 
 // standard headers
 #include <stdint.h>
+#include <vector>
 
 // Apache log4cxx
 #include "log4cxx/logger.h"
@@ -32,6 +33,7 @@ extern "C"
 
 
 // memory layout
+// TODO: Do we need to have separate areas for code and heap?
 #define ADDR_MEM_START   0x00000000
 #define ADDR_MEM_END     0x00ffffff     // 16MB memory (because the M68000 only had 24 address bits)
 #define ADDR_HEAP_START  0x00000000
@@ -222,13 +224,16 @@ int main(int argc, char *argv[])
     {
         Poco::FileInputStream exe(argv[1]);
         Poco::BinaryReader::BinaryReader reader(exe, Poco::BinaryReader::BIG_ENDIAN_BYTE_ORDER);
-        uint32_t htype;
+        uint32_t btype;                                 // block type
+        uint32_t hnum = 0;                              // hunk number
+        uint32_t hloc = ADDR_CODE_START;                // hunk location relative to the base address g_mem
+        std::vector <uint32_t> hlocs;                   // mapping of hunk numbers to locations
         while (!reader.eof()) {
-            reader >> htype;
-            switch (htype)
+            reader >> btype;
+            switch (btype)
             {
                 case HUNK_HEADER:
-                    LOG4CXX_INFO(g_logger, "hunk type = HUNK_HEADER");
+                    LOG4CXX_INFO(g_logger, "hunk #" << hnum << ", block type = HUNK_HEADER");
                     uint32_t lword;
                     reader >> lword;
                     LOG4CXX_DEBUG(g_logger, "long words reserved for resident libraries: " << lword);
@@ -242,40 +247,69 @@ int main(int argc, char *argv[])
                     LOG4CXX_DEBUG(g_logger, "number of last hunk: " << lhunk);
                     for (int i = fhunk; i <= lhunk; i++) {
                         reader >> lword;
-                        LOG4CXX_DEBUG(g_logger, "size (in bytes) of hunk #" << i << ": " << lword * 4);
+                        LOG4CXX_DEBUG(g_logger, "size (in bytes) of hunk #" << i << " = " << lword * 4 << ", location = " << Poco::format("0x%08x", hloc));
+                        hlocs.push_back(hloc);
+                        hloc += lword * 4;
                     }
                     break;
 
                 case HUNK_CODE:
-                    LOG4CXX_INFO(g_logger, "hunk type = HUNK_CODE");
+                    LOG4CXX_INFO(g_logger, "hunk #" << hnum << ", block type = HUNK_CODE");
                     uint32_t nwords;
                     reader >> nwords;
-                    LOG4CXX_DEBUG(g_logger, "size (in bytes) of code hunk: " << nwords * 4);
-                    reader.readRaw((char *) g_mem + ADDR_CODE_START, nwords * 4);
-                    LOG4CXX_TRACE(g_logger, "hex dump of code:\n" << hexdump(g_mem + ADDR_CODE_START, nwords * 4));
+                    LOG4CXX_DEBUG(g_logger, "size (in bytes) of code block: " << nwords * 4);
+                    // TODO: We need to insert a subroutine call before the actual code and pass the CLI arguments
+                    reader.readRaw((char *) g_mem + hlocs[hnum], nwords * 4);
+                    LOG4CXX_TRACE(g_logger, "hex dump of block:\n" << hexdump(g_mem + hlocs[hnum], nwords * 4));
                     break;
 
                 case HUNK_DATA:
-                    LOG4CXX_INFO(g_logger, "hunk type = HUNK_DATA");
-                    LOG4CXX_ERROR(g_logger, "hunk type is not implemented");
+                    LOG4CXX_INFO(g_logger, "hunk #" << hnum << ", block type = HUNK_DATA");
+                    reader >> nwords;
+                    LOG4CXX_DEBUG(g_logger, "size (in bytes) of data block: " << nwords * 4);
+                    reader.readRaw((char *) g_mem + hlocs[hnum], nwords * 4);
+                    LOG4CXX_TRACE(g_logger, "hex dump of block:\n" << hexdump(g_mem + hlocs[hnum], nwords * 4));
                     break;
 
                 case HUNK_BSS:
-                    LOG4CXX_INFO(g_logger, "hunk type = HUNK_BSS");
-                    LOG4CXX_ERROR(g_logger, "hunk type is not implemented");
+                    LOG4CXX_INFO(g_logger, "hunk #" << hnum << ", block type = HUNK_BSS");
+                    LOG4CXX_ERROR(g_logger, "block type is not implemented");
+                    throw std::runtime_error ("block type not implemented");
                     break;
 
                 case HUNK_RELOC32:
-                    LOG4CXX_INFO(g_logger, "hunk type = HUNK_RELOC32");
-                    LOG4CXX_ERROR(g_logger, "hunk type is not implemented");
+                    LOG4CXX_INFO(g_logger, "hunk #" << hnum << ", block type = HUNK_RELOC32");
+                    uint32_t noffsets;
+                    while (true) {
+                        reader >> noffsets;
+                        if (noffsets == 0)
+                            break;
+
+                        uint32_t refhnum;
+                        reader >> refhnum;
+                        if (hnum > lhunk) {
+                            LOG4CXX_ERROR(g_logger, "reloc referring to hunk #" << refhnum << " found while executable contains only " << lhunk + 1 << " hunks");
+                            throw std::runtime_error ("bad executable");
+                        }
+
+                        uint32_t  offset;
+                        for (int i = 0; i < noffsets; i++) {
+                            reader >> offset;
+                            LOG4CXX_DEBUG(g_logger, "applying reloc referring to hunk #" << refhnum << ", offset = " << offset);
+                            // TODO: Is the byte order of the address correct?
+                            m68k_write_32(hlocs[hnum] + offset, m68k_read_32(hlocs[hnum] + offset) + hlocs[refhnum]);
+                        }
+                    }
+                    LOG4CXX_TRACE(g_logger, "hex dump of block after applying relocs:\n" << hexdump(g_mem + hlocs[hnum], nwords * 4));
                     break;
 
                 case HUNK_END:
-                    LOG4CXX_INFO(g_logger, "hunk type = HUNK_END");
+                    LOG4CXX_INFO(g_logger, "hunk #" << hnum << ", block type = HUNK_END");
+                    ++hnum;
                     break;
 
                 default:
-                    LOG4CXX_ERROR(g_logger, "unknown hunk type: " << htype);
+                    LOG4CXX_ERROR(g_logger, "unknown block type: " << btype);
             }
         }
     }
@@ -298,11 +332,13 @@ int main(int argc, char *argv[])
         *p = 0x714e;
 */
 
+/*
     m68k_init();
     m68k_set_cpu_type(M68K_CPU_TYPE_68000);
     m68k_pulse_reset();
     m68k_execute(1000);
     LOG4CXX_TRACE(g_logger, "stack after execution of the program:\n" << hexdump(g_mem + ADDR_STACK_END - 32, 32));
+*/
 
     return 0;
 }
