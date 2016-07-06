9b600825f0c30b963134868609763a4bf1d92293
Constantin Wiemer <constantin.wiemer@gmx.de>
2016-07-06 14:31:40 +0200

    Recursively scanning sub-directories works now

--- amifind.c
+++ amifind.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 #include <proto/exec.h>
 #include <proto/dos.h>
 #include <exec/types.h>
 #include <exec/memory.h>
 #include <dos/dosextens.h>
+
+
+#define MAX_DEPTH 16
+#define MAX_PATH_LEN 1024
 
 
 int printf(const char *str, ...)
@@ -20,44 +25,51 @@ int printf(const char *str, ...)
 }
 
 
-void search(const char *path)
+void search(const char *dir)
 {
-    BPTR lock;
+    BPTR                   lock;
     struct FileInfoBlock *fib;
+    char                   newdir[MAX_PATH_LEN];
+    static unsigned int    depth = 0;
 
-    if ((lock = Lock (path, ACCESS_READ)) != 0) {
+    if ((lock = Lock (dir, ACCESS_READ)) != 0) {
         if ((fib = AllocVec(sizeof(struct FileInfoBlock), MEMF_CLEAR)) != NULL) {
             if (Examine(lock, fib)) {
                 if (fib->fib_DirEntryType > 0) {
-                    printf("examing directory '%s'\n", path);
-                    while (ExNext(lock, fib)) {
-                        if(fib->fib_DirEntryType > 0) {
-                            // another directory => call ourself recursively
-                            // TODO: add initial path
-                            search(fib->fib_FileName);
+                    ++depth;
+                    if (depth <= MAX_DEPTH) {
+                        printf("examing directory '%s' (depth = %d)\n", dir, depth);
+                        while (ExNext(lock, fib)) {
+                            if(fib->fib_DirEntryType > 0) {
+                                // another directory => call ourselves recursively
+                                strncpy(newdir, dir, MAX_PATH_LEN - 1);
+                                strncat(newdir, fib->fib_FileName, MAX_PATH_LEN - 1 - strlen(dir));
+                                search(newdir);
+                            }
+                            else {
+                                // plain file => just output file name and size for now
+                                printf("%-30s%-ld\n", fib->fib_FileName, fib->fib_Size);
+                            }
                         }
-                        else {
-                            // plain file => just output file name and size for now
-                            printf("%-30s%-ld\n", fib->fib_FileName, fib->fib_Size);
-                        }
+                        if (IoErr() != ERROR_NO_MORE_ENTRIES)
+                            printf("error occurred while examing directory '%s': %ld\n", dir, IoErr());
                     }
-                    if (IoErr() != ERROR_NO_MORE_ENTRIES)
-                        printf("error occurred while examing directory '%s': %ld\n", path, IoErr());
+                    else
+                        printf("maximum recursion depth reached - aborting\n");
                 }
-                else {
+                else
                     printf("search() was called on a file - aborting\n");
-                }
             }
             FreeVec(fib);
         }
-        else {
+        else
             printf("could not allocate memory for FileInfoBlock\n");
-        }
+
         UnLock(lock);
     }
-    else {
-        printf("could not obtain lock for directory %s\n", path);
-    }
+    else
+        printf("could not obtain lock for directory %s\n", dir);
+
     return;
 }
 
