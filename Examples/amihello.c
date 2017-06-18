bdbdffd69206fe175bdae20d025ea1f6001518ea
Constantin Wiemer <constantin.wiemer@gmx.de>
2017-06-18 22:13:20 +0200

    Renamed main function to main() because we link against libnix now

--- Examples/amihello.c
+++ Examples/amihello.c
@@ -1,8 +1,6 @@
 #include "proto/dos.h"
 
-// We must not name the function main, otherwise GCC will insert a call to __main which does apparently some
-// initializations for the standard library.
-int cwmain(int argc, char **argv)
+int main(int argc, char **argv)
 {
     PutStr("hello, amiga\n");
     Exit(0);
